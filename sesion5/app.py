f4709a23864b3802737ac6171ea66ce1c7c5c0a6
Susana Chang <samanta.chang@utec.edu.pe>
2022-04-18 22:40:30 -0500

    Add sesion5 v2

--- sesion5/app.py
+++ sesion5/app.py
@@ -1,26 +1,57 @@
 # imports
-from flask import Flask, render_template
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    url_for
+)
+
+from flask_sqlalchemy import SQLAlchemy
 
 
 # configurations
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost:5432/todoappdbp10'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+
+# models
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'Todo: id={self.id}, description={self.description}'
+
+db.create_all()
+
+
+# controllers
 @app.route('/')
 def index():
-    return render_template('index.html', data=[
-        {
-            'description':'todo 1'
-        },
-        {
-            'description':'todo 2'
-        },
-        {
-            'description':'todo 3'
-        },
-        {
-            'description':'todo 4'
-        }
-    ])
+    return render_template('index.html', data=Todo.query.all())
+
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    description = request.form.get('description', '')
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+@app.route('/todos/create', methods=['GET'])
+def create_todo_get():
+    description = request.args.get('description', '')
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
 
 # run
 if __name__ == '__main__':
